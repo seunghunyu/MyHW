65d81754366dd5e7989c4dbf0bb3b0dc3a992f2b
seunghunyu <livefor1212@gmail.com>
2018-06-14 00:44:26 +0900

    first version

---
+++ src/A.java
@@ -0,0 +1,9 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("클래스 A 입니다~~~~~");
+	}
+
+}
---
+++ src/C.java
@@ -0,0 +1,7 @@
+
+public class C {
+	public static void main(String [] args)
+	{
+		System.out.println("클래스 C 입니다!!!!! ");
+	}
+}
